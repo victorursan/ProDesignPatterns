04668d16b37e7b0a14bbb9434805e3ede9f8c265
Victor Ursan <victor.ursan@gmail.com>
2015-07-19 21:45:36 +0200

    reload the label

--- SportStore/SportStore/ViewController.swift
+++ SportStore/SportStore/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 class ViewController: UIViewController {
     
-    var products = [ (" Kayak", "A boat for one person", "Watersports", 275.0, 10),
+    @IBOutlet weak var totalStockLabel: UILabel!
+    
+    var products = [
+        (" Kayak", "A boat for one person", "Watersports", 275.0, 10),
         (" Lifejacket", "Protective and fashionable", "Watersports", 48.95, 14),
         (" Soccer Ball", "FIFA-approved size and weight", "Soccer", 19.5, 32),
         (" Corner Flags", "Give your playing field a professional touch", "Soccer", 34.95, 1),
@@ -17,13 +20,20 @@ class ViewController: UIViewController {
         (" Thinking Cap", "Improve your brain efficiency by 75%", "Chess", 16.0, 8),
         (" Unsteady Chair", "Secretly give your opponent a disadvantage", "Chess", 29.95, 3),
         (" Human Chess Board", "A fun game for the family", "Chess", 75.0, 2),
-        (" Bling-Bling King", "Gold-plated, diamond-studded King", "Chess", 1200.0, 4)];
+        (" Bling-Bling King", "Gold-plated, diamond-studded King", "Chess", 1200.0, 4)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        displayStockTotal()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func displayStockTotal() {
+        let stockTotal = products.reduce(0) {(total, product) -> Int in return total + product.4 }
+        totalStockLabel.text = "\( stockTotal) Products in Stock"
+    }
+    
 }
