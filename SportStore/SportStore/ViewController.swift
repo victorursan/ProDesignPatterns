8c7e4875b3319ad9e0a74d8870a1c2038ed13e87
Victor Ursan <victor.ursan@gmail.com>
2015-07-22 22:32:19 +0200

    refactored th ViewController

--- SportStore/SportStore/ViewController.swift
+++ SportStore/SportStore/ViewController.swift
@@ -14,7 +14,7 @@ class ProductTableCell: UITableViewCell {
     @IBOutlet weak var stockStepper: UIStepper!
     @IBOutlet weak var stockField: UITextField!
     
-    var productId: Int?
+    var product: Product?
 }
 
 class ViewController: UIViewController, UITableViewDataSource {
@@ -23,15 +23,15 @@ class ViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
     var products = [
-        (" Kayak", "A boat for one person", "Watersports", 275.0, 10),
-        (" Lifejacket", "Protective and fashionable", "Watersports", 48.95, 14),
-        (" Soccer Ball", "FIFA-approved size and weight", "Soccer", 19.5, 32),
-        (" Corner Flags", "Give your playing field a professional touch", "Soccer", 34.95, 1),
-        (" Stadium", "Flat-packed 35,000-seat stadium", "Soccer", 79500.0, 4),
-        (" Thinking Cap", "Improve your brain efficiency by 75%", "Chess", 16.0, 8),
-        (" Unsteady Chair", "Secretly give your opponent a disadvantage", "Chess", 29.95, 3),
-        (" Human Chess Board", "A fun game for the family", "Chess", 75.0, 2),
-        (" Bling-Bling King", "Gold-plated, diamond-studded King", "Chess", 1200.0, 4)]
+        Product(name:" Kayak", description:" A boat for one person", category:" Watersports", price: 275.0, stockLevel: 10),
+        Product(name:" Lifejacket", description:" Protective and fashionable", category:" Watersports", price: 48.95, stockLevel: 14),
+        Product(name:" Soccer Ball", description:" FIFA-approved size and weight", category:" Soccer", price: 19.5, stockLevel: 32),
+        Product(name:" Corner Flags", description:" Give your playing field a professional touch", category:" Soccer", price: 34.95, stockLevel: 1),
+        Product(name:" Stadium", description:" Flat-packed 35,000-seat stadium", category:" Soccer", price: 79500.0, stockLevel: 4),
+        Product(name:" Thinking Cap", description:" Improve your brain efficiency by 75%", category:" Chess", price: 16.0, stockLevel: 8),
+        Product(name:" Unsteady Chair", description:" Secretly give your opponent a disadvantage", category: "Chess", price: 29.95, stockLevel: 3),
+        Product(name:" Human Chess Board", description:" A fun game for the family", category:" Chess", price: 75.0, stockLevel: 2),
+        Product(name:" Bling-Bling King", description:" Gold-plated, diamond-studded King", category:" Chess", price: 1200.0, stockLevel: 4)];
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,19 +47,16 @@ class ViewController: UIViewController, UITableViewDataSource {
             while (true) {
                 currentCell = currentCell.superview!
                 if let cell = currentCell as? ProductTableCell {
-                    if let id = cell.productId {
-                        var newStockLevel:Int?
+                    if let product = cell.product {
                         if let stepper = sender as? UIStepper {
-                            newStockLevel = Int(stepper.value)
-                        } else if let textfield = sender as? UITextField,
-                            newValue = textfield.text.toInt() {
-                            newStockLevel = newValue
+                            product.stockLevel = Int(stepper.value)
+                        } else if let textfield = sender as? UITextField {
+                            if let newValue = textfield.text.toInt() {
+                                product.stockLevel = newValue
+                            }
                         }
-                        if let level = newStockLevel {
-                            products[id].4 = level
-                            cell.stockStepper.value = Double(level)
-                            cell.stockField.text = String(level)
-                        }
+                        cell.stockStepper.value = Double( product.stockLevel)
+                        cell.stockField.text = String( product.stockLevel)
                     }
                     break
                 }
@@ -69,8 +66,12 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func displayStockTotal() {
-        let stockTotal = products.reduce(0) {(total, product) -> Int in return total + product.4 }
-        totalStockLabel.text = "\(stockTotal) Products in Stock"
+        let finalTotals:(Int, Double) = products.reduce(( 0, 0.0)) {
+            ( totals, product) -> (Int, Double) in
+            return ( totals.0 + product.stockLevel, totals.1 + product.stockValue )
+        }
+        totalStockLabel.text = "\(finalTotals.0) Products in Stock" +
+        " Total Value \(Utils.currencyStringFromNumber(finalTotals.1))"
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -80,14 +81,12 @@ class ViewController: UIViewController, UITableViewDataSource {
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let product = products[indexPath.row]
         let cell = tableView.dequeueReusableCellWithIdentifier("ProductCell") as! ProductTableCell
-        cell.productId = indexPath.row
-        cell.nameLabel.text = product.0
-        cell.descriptionLabel.text = product.1
-        cell.stockStepper.value = Double(product.4)
-        cell.stockField.text = String(product.4)
+        cell.product = products[indexPath.row]
+        cell.nameLabel.text = product.name
+        cell.descriptionLabel.text = product.description
+        cell.stockStepper.value = Double(product.stockLevel)
+        cell.stockField.text = "\(product.stockLevel)"
         return cell;
     }
-    
-    
     
 }
