07a5324e49b32861b380b5b52d608922ca49232f
Victor Ursan <victor.ursan@gmail.com>
2015-07-25 19:56:31 +0200

    fixed concurency issue

--- SportStore/SportStore/Logger.swift
+++ SportStore/SportStore/Logger.swift
@@ -12,13 +12,21 @@ class Logger<T where T:NSObject, T:NSCopying> {
     var dataItems:[T] = []
     var callback:(T) -> ()
     var arrayQ = dispatch_queue_create("arrayQ", DISPATCH_QUEUE_CONCURRENT)
+    var callbackQ = dispatch_queue_create("callbackQ", DISPATCH_QUEUE_SERIAL)
     
-    init(callback:(T) -> ()) {
+    init(callback:(T) -> (), protected: Bool = true) {
         self.callback = callback
+        if (protected) {
+            self.callback = {(item: T) in
+                dispatch_sync(self.callbackQ) {
+                    callback(item)
+                }
+            }
+        }
     }
     
     func logItem(item: T) {
-        dispatch_sync(arrayQ) {
+        dispatch_barrier_async(arrayQ) {
             self.dataItems.append(item.copy() as! T)
             self.callback(item)
         }
