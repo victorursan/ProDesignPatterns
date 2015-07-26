d2f7a0e890ca58bf7c16fe5f725c9cb87cc30055
Victor Ursan <victor.ursan@gmail.com>
2015-07-26 13:38:11 +0200

    refactored the ViewController

--- SportStore/SportStore/NetworkConnection.swift
+++ SportStore/SportStore/NetworkConnection.swift
@@ -11,7 +11,7 @@ import Foundation
 class NetworkConnection {
     
     private let stockData: [String: Int] = ["Kayak" : 10, "Lifejacket": 14,
-        "Soccer Ball": 32," Corner Flags": 1, "Stadium": 4, "Thinking Cap": 8,
+        "Soccer Ball": 32, "Corner Flags": 1, "Stadium": 4, "Thinking Cap": 8,
         "Unsteady Chair": 3, "Human Chess Board": 2, "Bling-Bling King": 4 ];
     
     func getStockLevel(name: String) -> Int? {
--- SportStore/SportStore/ViewController.swift
+++ SportStore/SportStore/ViewController.swift
@@ -23,21 +23,22 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var totalStockLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
-    
-    var products = [
-        Product(name: "Kayak", description: "A boat for one person", category: "Watersports", price: 275.0, stockLevel: 10),
-        Product(name: "Lifejacket", description: "Protective and fashionable", category: "Watersports", price: 48.95, stockLevel: 14),
-        Product(name: "Soccer Ball", description: "FIFA-approved size and weight", category: "Soccer", price: 19.5, stockLevel: 32),
-        Product(name: "Corner Flags", description: "Give your playing field a professional touch", category: "Soccer", price: 34.95, stockLevel: 1),
-        Product(name: "Stadium", description: "Flat-packed 35,000-seat stadium", category: "Soccer", price: 79500.0, stockLevel: 4),
-        Product(name: "Thinking Cap", description: "Improve your brain efficiency by 75%", category: "Chess", price: 16.0, stockLevel: 8),
-        Product(name: "Unsteady Chair", description: "Secretly give your opponent a disadvantage", category: "Chess", price: 29.95, stockLevel: 3),
-        Product(name: "Human Chess Board", description: "A fun game for the family", category: "Chess", price: 75.0, stockLevel: 2),
-        Product(name: "Bling-Bling King", description: "Gold-plated, diamond-studded King", category: "Chess", price: 1200.0, stockLevel: 4)];
+    var productStore = ProductDataStore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         displayStockTotal()
+        productStore.callback = {(product: Product) in
+            self.tableView.visibleCells().map{(cell: AnyObject) -> () in
+                if let productCell = cell as? ProductTableCell {
+                    if productCell.product?.name == product.name {
+                        productCell.stockStepper.value = Double(product.stockLevel)
+                        productCell.stockField.text = String(product.stockLevel)
+                    }
+                }
+            }
+            self.displayStockTotal();
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -69,7 +70,7 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func displayStockTotal() {
-        let finalTotals:(Int, Double) = products.reduce(( 0, 0.0)) {
+        let finalTotals:(Int, Double) = productStore.products.reduce(( 0, 0.0)) {
             ( totals, product) -> (Int, Double) in
             return ( totals.0 + product.stockLevel, totals.1 + product.stockValue )
         }
@@ -78,13 +79,13 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return products.count
+        return productStore.products.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let product = products[indexPath.row]
+        let product = productStore.products[indexPath.row]
         let cell = tableView.dequeueReusableCellWithIdentifier("ProductCell") as! ProductTableCell
-        cell.product = products[indexPath.row]
+        cell.product = product
         cell.nameLabel.text = product.name
         cell.descriptionLabel.text = product.productDescription
         cell.stockStepper.value = Double(product.stockLevel)
