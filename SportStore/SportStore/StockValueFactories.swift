9187bed09e26c359bb6bcf6797f35ec51f466631
Victor Ursan <victor.ursan@gmail.com>
2015-08-09 22:55:01 +0200

    added StockValueFactories

---
+++ SportStore/SportStore/StockValueFactories.swift
@@ -0,0 +1,62 @@
+//
+//  StockValueFactories.swift
+//  SportStore
+//
+//  Created by Victor Ursan on 8/9/15.
+//  Copyright (c) 2015 victorursan. All rights reserved.
+//
+
+import Foundation
+
+class StockTotalFactory {
+    enum Currency {
+        case USD
+        case GBP
+    }
+    
+    private(set) var formatter: StockValueFormatter?
+    private(set) var converter: StockValueConverter?
+    
+    class func getFactory(curr: Currency) -> StockTotalFactory {
+        if (curr == Currency.USD) {
+            return DollarStockTotalFactory.sharedInstance
+        } else {
+            return PoundStockTotalFactory.sharedInstance
+        }
+    }
+}
+
+private class DollarStockTotalFactory: StockTotalFactory {
+    private override init() {
+        super.init()
+        formatter = DollarStockValueFormatter()
+        converter = DollarStockValueConverter()
+    }
+    
+    class var sharedInstance: StockTotalFactory {
+        get {
+            struct SingletonWrapper {
+                static let singleton = DollarStockTotalFactory()
+            }
+            return SingletonWrapper.singleton
+        }
+    }
+}
+
+private class PoundStockTotalFactory: StockTotalFactory {
+    private override init() {
+        super.init()
+        formatter = PoundStockValueFormatter()
+        converter = PoundStockValueConverter()
+    }
+    
+    class var sharedInstance: StockTotalFactory {
+        get {
+            struct SingletonWrapper {
+                static let singleton = PoundStockTotalFactory()
+            }
+            return SingletonWrapper.singleton
+        }
+    }
+}
+
