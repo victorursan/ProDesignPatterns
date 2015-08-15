8841bf046adbef309c728a4c8881901038db9d24
Victor Ursan <victor.ursan@gmail.com>
2015-08-15 14:21:06 +0200

    added Euro module with EuroHandler class

---
+++ SportStore/SportStore/Euro.swift
@@ -0,0 +1,21 @@
+//
+//  Euro.swift
+//  SportStore
+//
+//  Created by Victor Ursan on 8/15/15.
+//  Copyright (c) 2015 victorursan. All rights reserved.
+//
+
+import Foundation
+
+class EuroHandler {
+    
+    func getDisplaytring(amount: Double) -> String {
+        let formater = Utils.currencyStringFromNumber(amount)
+        return "€\(dropFirst(formater))"
+    }
+    
+    func getCurrencyAmount(amount: Double) -> Double {
+        return amount * 0.76164
+    }
+}
